7d4636e88f694be655925865e1eaaf7e9df9c6c9
anorld-droid <mulindipatrice00@gmail.com>
2022-04-28 13:39:05 +0300

    Made migrations , commented out secret key for production reasons.

--- Aps_api/settings.py
+++ Aps_api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^a8%uth)=q2k^uv9$n!_ffe!c2ipcocj89j8nr_w7gsvg9zstq'
+# SECRET_KEY = 'django-insecure-^a8%uth)=q2k^uv9$n!_ffe!c2ipcocj89j8nr_w7gsvg9zstq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
