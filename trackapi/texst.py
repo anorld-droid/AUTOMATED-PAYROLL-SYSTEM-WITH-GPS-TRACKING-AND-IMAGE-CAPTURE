ce619c021b4d991e2744f6ef0f1ea2dc03de5080
anorld-droid <mulindipatrice00@gmail.com>
2022-05-29 15:50:56 +0300

    unwanted file

--- trackapi/texst.py
+++
@@ -1,8 +0,0 @@
-import datetime
-tnow = datetime.datetime.now()
-y = tnow.date().year
-m = tnow.date().month
-d = tnow.date().day
-if datetime.datetime(y, m, d, 18, 00, 00, 0000) <= tnow:
-    print("me")
-print("you")
